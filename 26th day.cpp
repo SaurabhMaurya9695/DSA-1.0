12836c2c24e917fba529708993366baf5c8dd9bd
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-02-22 22:34:15 +0530

    peak element II and 2D matrix

---
+++ 26th day.cpp
@@ -0,0 +1,89 @@
+						// Search on 2D matrix I & II same code 
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size() ;
+        int col = matrix[0].size() ;
+        int i = 0;
+        int j = col - 1 ;
+        while(i < row && j >= 0){
+            if(matrix[i][j] == target){
+                return true;
+            }
+            else if(matrix[i][j] > target){
+                j -- ;
+            }
+            else{
+                i ++ ;
+            }
+        }
+        
+        return false;
+        
+    }
+};
+						//Peak element II
+
+class Solution {
+public:
+    
+    int MaxValueIdx(vector<int> arr){
+        // cout<<"Cha" << endl;
+        int max = 0 ;
+        int maxidx = 0;
+        for(int i = 0 ; i < arr.size() ; i ++){
+            if(arr[i] > max){
+                maxidx = i ;
+                max = arr[i] ;
+            }
+        }
+        return maxidx ;
+    }
+    
+    vector<int> findPeakGrid(vector<vector<int>>& arr) {
+        int row = arr.size() ;
+        int col = arr[0].size() ;
+        int start = 0 , end = row - 1;
+        while(start <= end){
+            int middlerow = (start + end) / 2; 
+            //first we have to find maxValueIndex of in middleRow
+            int rowMaxidx =  MaxValueIdx(arr[middlerow] ) ;
+            
+            // our middle row is top row then our upper row doesn't exist 
+            if(middlerow == 0){
+                if(arr[middlerow][rowMaxidx] > arr[middlerow + 1][rowMaxidx]){
+                    return {middlerow , rowMaxidx};
+                }
+            }
+            
+            //our middle row is top row then our uplower  row doesn't exist
+            if(middlerow == row - 1){
+                if(arr[middlerow][rowMaxidx] > arr[middlerow - 1][rowMaxidx]){
+                    return {middlerow , rowMaxidx};
+                }
+            }
+            
+            //checking the max element whether  the elem is gret then its side 
+            if(
+                arr[middlerow][rowMaxidx] > arr[middlerow + 1][rowMaxidx]  && 
+                (arr[middlerow][rowMaxidx] > arr[middlerow - 1][rowMaxidx])
+            ){
+                    return {middlerow , rowMaxidx} ;
+                
+            }
+            
+            //where we have to go now ? -> up or dowm 
+            if(arr[middlerow][rowMaxidx]  < arr[middlerow + 1][rowMaxidx] ){
+                // we have to go in 
+                start = middlerow + 1;
+            }
+            else{
+                end = middlerow - 1;
+            }
+            
+            
+        }
+        // cout << MaxValueIdx(arr[1]) << endl ;
+        return {-1,-1} ;
+    }
+};
